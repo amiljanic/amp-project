8186d6f4b45fd72774c79bece26845e7668d756a
Ana Miljanic <ana.miljanic1234@gmail.com>
2020-04-18 18:53:29 +0200

    Add Customer model, migrations, seeder, factory, controller

---
+++ app/Http/Controllers/Api/CustomersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Customer;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show(Customer $customer)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, Customer $customer)
+    {
+
+    }
+
+    public function destroy(Customer $customer)
+    {
+
+    }
+}
---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Customer;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Customer::class, function (Faker $faker) {
+    return [
+        'user_id' => factory(User::class),
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'address' => $faker->address,
+    ];
+});
