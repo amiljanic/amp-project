1400b03d4c932134336f247bb20c52c787e3fc97
Ana Miljanic <ana.miljanic1234@gmail.com>
2020-04-18 18:55:57 +0200

    Add Product model, migrations, controller

---
+++ database/migrations/2020_04_18_141122_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->string('name');
+            $table->string('description');
+            $table->text('details');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('seller_id')
+                ->on('sellers')
+                ->references('id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
